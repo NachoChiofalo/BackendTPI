ebe51a9a05dba8c14709c7c04a0b217ad08b9bb2
nacho <nachochiofalo@gmail.com>
2025-11-12 22:21:07 -0300

    OSRM agregado(sin mapa)

---
+++ ms-localizaciones/src/main/java/com/tpi/localizaciones/controller/GeoController.java
@@ -0,0 +1,25 @@
+package com.tpi.localizaciones.controller;
+
+import com.tpi.localizaciones.dto.DistanciaDTO;
+import com.tpi.localizaciones.service.GeoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/distancia")
+@RequiredArgsConstructor
+public class GeoController {
+
+    private final GeoService geoService;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public DistanciaDTO obtenerDistancia(
+            @RequestParam String origen,
+            @RequestParam String destino) throws Exception {
+        return geoService.calcularDistancia(origen, destino);
+    }
+}
